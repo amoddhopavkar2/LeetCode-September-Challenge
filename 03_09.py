9500a1091e0c312b62e6015980332075c5852a55
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-09-03 12:59:33 +0530

    03_09.py

---
+++ 03_09.py
@@ -0,0 +1,16 @@
+# Repeated Substring Pattern
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+    	string_len = len(s)
+    	for i in range(string_len // 2, 0, -1):
+    		if string_len % i == 0:
+    			repeats = string_len // i
+    			new_string = ""
+    			sub_string = s[:i]
+    			for j in range(repeats):
+    				new_string += sub_string
+
+    			if new_string == s:
+    				return True
+    	return False
