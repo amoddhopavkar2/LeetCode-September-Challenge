7e62711abba9bc670c521a6f987ead3d4a3d6ba6
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-09-06 11:31:52 +0530

    05_09.py

---
+++ 05_09.py
@@ -0,0 +1,16 @@
+# All elements is a BST
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        def traverse(node):
+            if not node:
+                return []
+            return traverse(node.left) + [node.val] + traverse(node.right)
+
+        return sorted(traverse(root1) + traverse(root2))
