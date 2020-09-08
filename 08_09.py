5a4deaa8a22cd40e3b32907514c81787bf1cc879
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-09-08 17:29:36 +0530

    update 08_09.py

--- 08_09.py
+++ 08_09.py
@@ -8,21 +8,22 @@
 #         self.right = right
 class Solution:
     def sumRootToLeaf(self, root: TreeNode) -> int:
-    	def traversal(root, path, path_len, all_paths):
-    		if not root:
-    			return
-    		if len(path) > path_len:
-    			path[path_len] = root.val
-    		else:
-    			path.append(root.val)
+        def traversal(root, path, path_len, all_paths):
+            if not root:
+                return
+            if len(path) > path_len:
+                path[path_len] = root.val
+            else:
+                path.append(root.val)
 
-    		path_len += 1
-    		if not root.left and not root.right:
-    			all_paths.append(int(''.join(str(val) for val in path[:path_len]), 2))
-    		else:
-    			traversal(root.left, path, path_len, all_paths)
-    			traversal(root.right, path, path_len, all_paths)
-    	
+            path_len += 1
+            if not root.left and not root.right:
+                all_paths.append(int(''.join(str(val)
+                                             for val in path[:path_len]), 2))
+            else:
+                traversal(root.left, path, path_len, all_paths)
+                traversal(root.right, path, path_len, all_paths)
+
         path = []
-    	traversal(root, [], 0, path)
-    	return sum(path)
+        traversal(root, [], 0, path)
+        return sum(path)
