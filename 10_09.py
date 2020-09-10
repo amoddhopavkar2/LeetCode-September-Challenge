bd423932ce8e9500528eec0f65170c6c27c8f478
amoddhopavkar2 <amoddhopavkar2@gmail.com>
2020-09-10 13:00:57 +0530

    add 10_09.py

---
+++ 10_09.py
@@ -0,0 +1,21 @@
+# Bulls and Cows
+
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+    	bulls, cows = 0, 0
+    	new_guess = []
+    	new_secret = []
+
+    	for i in range(len(secret)):
+    		if secret[i] == guess[i]:
+    			bulls += 1
+    		else:
+    			new_secret.append(secret[i])
+    			new_guess.append(guess[i])
+
+    	for key in new_guess:
+    		if key in new_secret:
+    			cows += 1
+    			new_secret.remove(key)
+
+    	return("{}A{}B".format(bulls, cows))
