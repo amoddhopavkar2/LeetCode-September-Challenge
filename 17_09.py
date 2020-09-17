974e24f8b780a5b272e19d0afcf58fba8928d9bd
amoddhopavkar2 <amoddhopavkar2@gmail.com>
2020-09-17 13:27:00 +0530

    add 17_09.py

---
+++ 17_09.py
@@ -0,0 +1,17 @@
+# Round Bounded in Circle
+
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        i, j, di, dj = 0, 0, 0, 1
+
+        for move in instructions:
+            if move == 'G':
+                i, j = i+di, j+dj
+
+            elif move == 'L':
+                di, dj = -dj, di
+
+            else:
+                di, dj = dj, -di
+
+        return i == j == 0 or (di, dj) != (0, 1)
