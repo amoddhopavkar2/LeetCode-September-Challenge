7a0b2078f799fda80c4e69b5cc8b8e389743f214
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-09-25 13:09:43 +0530

    add 25_09.[y

---
+++ 25_09.py
@@ -0,0 +1,26 @@
+# Largest Number
+
+class Solution:
+	def largestNumber(self, nums: List[int]) -> str:
+		if len(nums) == 0:
+			return ''
+
+		result = ''
+		count = 0
+
+		for x in range(0, len(nums)-1):
+			for y in range(x+1, len(nums)):
+				a, b = str(nums[x]), str(nums[y])
+				if int(a + b) < int(b + a):
+					temp = nums[x]
+					nums[x] = nums[y]
+					nums[y] = temp
+
+		if nums[0] == 0:
+			return '0'
+
+		while count < len(nums):
+			result += str(nums[count])
+			count += 1
+
+		return result
